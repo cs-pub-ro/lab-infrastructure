e3ec34fce7409b0d70d3f7a85d53a7708ba49168
Mihai-Valentin DUMITRU <mihai.dumitru@cs.pub.ro>
2019-09-21 18:20:56 +0300

    add install-apt-packages script

---
+++ install/install-apt-packages.bash
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+
+install_apt_package() {
+	sudo apt -y install "$1"
+}
+
+# Update apt-file cache.
+sudo apt update
+
+for package_file; do
+	while read package; do
+		# allow comments and empty lines in packages file
+		if [[ "$package" =~ "#"  || -z "$package" ]]; then
+			continue
+		fi
+
+		install_apt_package "$package"
+	done < "$package_file"
+done
